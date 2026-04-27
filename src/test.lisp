c7d0ee63ac159c50c7546f27fb54b30efcd37598
Jason Walker <JasonW94@gmail.com>
2026-04-27 18:22:53 -0400

    Added delimited continuations

--- src/test.lisp
+++ src/test.lisp
@@ -14,22 +14,33 @@
 
 (cl:in-package #:indexed-monads/test)
 
+(cl:defun test1 ()
+  (coalton
+   (run-io!
+    (eval-contT
+     (do
+      (pt-lift (write "Enter a number: "))
+      (input <- (pt-lift read-line))
+       (call-cc
+        (fn (resume)
+          (do
+           (let result? = (parse-int input))
+           (do-when-match result? (None)
+             (pt-lift (write-line "Not a valid number"))
+             (resume Unit))
+            (do-when-match result? (Some x)
+              (do-when (< x 0)
+                (pt-lift (write-line "Number less than zero"))
+                (resume Unit))
+              (pt-lift (write-line "Number >= 0"))))))
+       (pure Unit))))))
+
 (coalton
- (run-io!
-  (eval-contT
-   (do
-    (pt-lift (write "Enter a number: "))
-    (input <- (pt-lift read-line))
-    (call-cc
-     (fn (resume)
-       (do
-        (let result? = (parse-int input))
-        (do-when-match result? (None)
-          (pt-lift (write-line "Not a valid number"))
-          (resume Unit))
-        (do-when-match result? (Some x)
-          (do-when (< x 0)
-            (pt-lift (write-line "Number less than zero"))
-            (resume Unit))
-          (pt-lift (write-line "Number >= 0"))))))
-    (pure Unit)))))
+ (the (List String)
+      (reset
+       (pure (make-list "10")))))
+
+        ;; (x <- (shift
+        ;;        (fn (k)
+        ;;          (make-list (k 1) (k 2) (k 3)))))
+        ;; (pure (* x 10)))))
